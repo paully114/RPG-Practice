da629245dcdb91fb9da2ff9b0ca1e04343b0a981
paully114 <34038588+paully114@users.noreply.github.com>
2017-11-28 02:15:17 +0800

    Initial commit

---
+++ Script/Intearactable.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intearactable : MonoBehaviour
+{
+
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+
+    bool isFocus = false;
+    Transform player;
+
+    private bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+        
+    }
+}
---
+++ Script/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask movementMask;
+    PlayerMotor motor;
+    Camera cam;
+
+    public Intearactable focus;
+    // Use this for initialization
+    void Start()
+    {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+
+                RemoveFocus();
+            }
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Intearactable interactable = hit.collider.GetComponent<Intearactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void SetFocus(Intearactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus != null)
+                focus.OnDefocused();
+
+            
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+
+        
+        newFocus.OnFocused(transform);
+       
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+          focus.OnDefocused();
+
+        focus = null;
+        motor.StopFollowingTarget();
+    }
+}
+
